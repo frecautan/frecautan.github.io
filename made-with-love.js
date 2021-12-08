44bc2746f5d230ca7f6fb47b33ba9c79ef144a56
frecautan <93873911+frecautan@users.noreply.github.com>
2021-12-08 18:28:12 +0200

    Update made-with-love.js

--- made-with-love.js
+++ made-with-love.js
@@ -1,2 +1,4 @@
-const body = document.querySelector("body");
-body.insertAdjacentHTML('beforeend', '<div id="two"><h5>Made with love</h5></div>');
+document.addEventListener("DOMContentLoaded", function (event) {
+    const body = document.querySelector("body");
+    body.insertAdjacentHTML('beforeend', '<h5 style="color: red;">Designed & Developed with love by Igor Frecautan</h5>');
+});
