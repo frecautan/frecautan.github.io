dff2819250ca2e864ace8d07c3e6804e3f215782
Igor Frecautan <frecautan.igor@gmail.com>
2021-12-09 13:49:46 +0200

    First commit

--- made-with-love.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-    const body = document.querySelector("body");
-    body.insertAdjacentHTML('beforeend', '<h5 style="color: red;">Designed & Developed with love by Igor Frecautan</h5>');
-});
